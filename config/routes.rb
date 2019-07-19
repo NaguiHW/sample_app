95e3ace5d9ed3a91acd9741ad1f0a909f43103dd
Kalib Hackin <nagui08@hotmail.com>
2019-07-19 12:54:14 -0500

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   post '/signup',  to: 'users#create'
   resources :users
+  resources :account_activations, only: [:edit]
 end
